8f4740bd164e948c4cc8b8f50357fe97410df4f0
Nathan <nathan.arago@gmail.com>
2021-04-16 12:46:55 +0200

    Created empty card script

---
+++ cards/card.gd
@@ -0,0 +1,15 @@
+extends Node
+
+class_name Card
+
+var price : int
+var title : String
+var description : String
+var background_path : String
+
+# The picture on the card
+var foreground_path : String
+
+
+func use() -> void:
+	pass

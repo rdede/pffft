bc0730b8c7d5bc2b73245b67a065b342c48e8226
Nathan <nathan.arago@gmail.com>
2021-04-20 23:45:24 +0200

    Add card use drop

--- cards/card.gd
+++ cards/card.gd
@@ -8,35 +8,41 @@ var description := "Description comming soon"
 var background_path : String = ""
 # The picture on the card
 var foreground_path : String = ""
+
 var target_pos := Vector2.ZERO
 const HOVER_ZOOM_FACTOR := 1.25
 const DRAW_TIME := 0.2
 var t := 0.0
 
-enum STATES {IN_HAND, HOVERED, IN_MOUSE, REORGANIZE}
+var is_hovered := false
+var wait_to_remove := false
+
+enum STATES {IN_HAND, IN_MOUSE, REORGANIZE}
 var current_state = STATES.IN_HAND
 
 var mousepos_offset := Vector2.ZERO
+
+signal droped
+signal used
 
 func _ready() -> void:
 	check_init_error()
 	current_state = STATES.IN_HAND
 	
 func _process(delta: float) -> void:
-	if current_state == STATES.HOVERED and Input.is_action_pressed("Player_attack"):
+	if is_hovered and Input.is_action_pressed("Player_attack"):
 		current_state = STATES.IN_MOUSE
+		is_hovered = false
 		mousepos_offset = get_global_mouse_position() - rect_global_position
 		
 	if current_state == STATES.IN_MOUSE and Input.is_action_just_released("Player_attack"):
 		rect_scale = Vector2.ONE * 1
 		current_state = STATES.REORGANIZE
+		emit_signal("droped", get_global_mouse_position(), self)
 
 func _physics_process(delta: float) -> void:
-
 	match current_state:
 		STATES.IN_HAND:
-			pass
-		STATES.HOVERED:
 			pass
 		STATES.IN_MOUSE:
 			var mousepos := get_global_mouse_position()
@@ -49,10 +55,10 @@ func _physics_process(delta: float) -> void:
 				rect_position = target_pos
 				t = 0.0
 				current_state = STATES.IN_HAND
-				print_debug("In hand")
+
 func use() -> void:
-	#Activate the power of the card
-	pass
+	wait_to_remove = true
+	emit_signal("used", self)
 
 func check_init_error() -> void: 
 	# Price errors
@@ -73,10 +79,12 @@ func check_init_error() -> void:
 
 func _on_ClickZone_mouse_entered() -> void:
 	rect_scale = Vector2.ONE * HOVER_ZOOM_FACTOR
-	if current_state != STATES.REORGANIZE:
-		current_state = STATES.HOVERED
+	is_hovered = true
 
 func _on_ClickZone_mouse_exited() -> void:
 	rect_scale = Vector2.ONE * 1
-	if current_state != STATES.REORGANIZE:	
+	is_hovered = false
+	if rect_global_position != target_pos:
+		current_state = STATES.REORGANIZE
+	else:
 		current_state = STATES.IN_HAND
--- cards/player_hand.gd
+++ cards/player_hand.gd
@@ -4,10 +4,12 @@ var CardBase = preload("res://cards/card.tscn")
 var cards := 0 
 var card_in_mouse : Card = null
 var card_offset := 10.0
-enum CARD_STATES {IN_HAND, HOVERED, IN_MOUSE, REORGANIZE}
+enum CARD_STATES {IN_HAND, IN_MOUSE, REORGANIZE}
 
-onready var center_card_hand := get_viewport().size * Vector2(0.5, 1.2)
+onready var center_card_hand := get_viewport().size * Vector2(0.5, 1) - Vector2(0, 130)
 onready var hand := $Cards
+
+signal card_dropped
 
 func _process(delta: float) -> void:
 	#Check if a card is in mouse
@@ -22,9 +24,6 @@ func _process(delta: float) -> void:
 		organize_cards()
 		card_in_mouse = card_in_mouse_temp
 	
-func _ready() -> void:
-	pass # Replace with function body.
-	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Secondary_Click"):
 		draw_card()
@@ -33,12 +32,16 @@ func draw_card() -> void:
 	cards += 1
 	var new_card = CardBase.instance()
 	hand.add_child(new_card)
+	new_card.connect("droped", self, "_on_Card_dropped")
+	new_card.connect("used", self, "_on_Card_used")
 	organize_cards()
 
 func organize_cards() -> void:
 	var cards_processed := 0
 	var cards_in_hand := cards
 	for card in hand.get_children():
+		if card.wait_to_remove:
+			continue
 		if card.current_state == CARD_STATES.IN_MOUSE:
 			cards_in_hand -= 1
 			continue
@@ -47,3 +50,12 @@ func organize_cards() -> void:
 		x_pos -= card.rect_size.x * cards_processed + card_offset * cards_processed
 		card.target_pos = Vector2(round(x_pos), round(center_card_hand.y))
 		cards_processed += 1
+
+func _on_Card_dropped(cursor_position: Vector2, card: Card) -> void:
+	emit_signal("card_dropped", cursor_position, card)
+
+func _on_Card_used(card: Card) -> void:
+	card.queue_free()
+	card_in_mouse = null
+	cards -= 1
+	organize_cards()
